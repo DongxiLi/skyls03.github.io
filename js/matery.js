9af9f56275032be2ae744c2b6c3e7ebf7554d518
skyls03 <skyls03@qq.com>
2019-11-13 17:58:03 +0800

    Site updated: 2019-11-13 17:57:56

--- js/matery.js
+++ js/matery.js
@@ -104,7 +104,6 @@ $(function () {
             selector: '.img-item',
             // 启用字幕
             subHtmlSelectorRelative: true
-            showThumbByDefault: false //这句加上
         });
         $(document).find('img[data-original]').each(function(){
             $(this).parent().attr("href", $(this).attr("data-original"));
